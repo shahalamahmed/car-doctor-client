afe0199f30f20892ddc647b261c2aa65e26d2ce8
shahalamahmed <fcshahamol@gmail.com>
2024-05-02 09:08:43 +0600

    added bannner section footer section

---
+++ src/pages/Shared/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link, NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+
+    const NavItems = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+
+    </>
+    return (
+        <div className="navbar bg-base-100 h-28 mb-4">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {NavItems}
+                    </ul>
+                </div>
+                <Link className="btn btn-ghost text-xl">logo</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {NavItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <button className="btn btn-outline btn-waring">Appointment</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
