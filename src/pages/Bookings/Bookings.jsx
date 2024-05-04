bba3d4f7f208f9b0fcb08b44cd7d29b398743aef
shahalamahmed <fcshahamol@gmail.com>
2024-05-04 23:36:40 +0600

    added new section

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import BookingsRow from "./BookingsRow";
+import axios from "axios";
 
 
 const Bookings = () => {
@@ -8,13 +9,17 @@ const Bookings = () => {
     const { user } = useContext(AuthContext);
 
     const [bookings, setBookings] = useState([]);
-    console.log(bookings);
 
-    const url = `http://localhost:5000/bookings?email=${user?.email}`
+
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setBookings(data))
+        axios.get(url , {withCredentials: true})
+        .then(res => {
+            setBookings(res.data)
+        })
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => setBookings(data))
     }, [url])
 
     const handleDelete = id => {
